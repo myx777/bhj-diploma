b307f82fa17eaa2c8869fc0fcf17eb1775fdaa53
MYX <it.anton22@gmail.com>
2023-10-23 20:30:25 +0300

    correct fetch

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,77 +3,78 @@
  * Основная функция для совершения запросов
  * на сервер.
  */
-const createRequest = (options = {}) => {
-  const xhr = new XMLHttpRequest;
+// const createRequest = (options = {}) => {
+//   const xhr = new XMLHttpRequest;
 
-  xhr.onload = function() {
-    if (xhr.status != 200) {
-      options.callback(new Error(`Ошибка ${xhr.status}: ${xhr.statusText}`));
-    } else {
-      options.callback(null, xhr.response);
-    }
-  }
+//   xhr.onload = function() {
+//     if (xhr.status != 200) {
+//       options.callback(new Error(`Ошибка ${xhr.status}: ${xhr.statusText}`));
+//     } else {
+//       options.callback(null, xhr.response);
+//     }
+//   }
 
-  xhr.onerror = function() {
-    options.callback(new Error(`Request failed!`));
-  }
+//   xhr.onerror = function() {
+//     options.callback(new Error(`Request failed!`));
+//   }
   
-  const urlGet = options.url;
-  let formData = null;
+//   const urlGet = options.url;
+//   let formData = null;
 
-  if (options.method === 'GET') {
-    urlGet.search = new URLSearchParams(options.data).toString();
-  } 
+//   if (options.method === 'GET') {
+//     urlGet.search = new URLSearchParams(options.data).toString();
+//   } 
 
-  if(options.method != 'GET'){
-    formData = new FormData();
-    for(const key in options.data){// для универсальности≤ чтобы не было привязки в data: {email: "email@.ru"}
-      formData.append(key, options.data[key]);
-    }
-  }
-  xhr.open(options.method, urlGet, true);
-  xhr.responseType = 'json';
-  xhr.send(formData);
-};
-
-/** 
+//   if(options.method !== 'GET'){
+//     formData = new FormData();
+//     for(const key in options.data){// для универсальности≤ чтобы не было привязки в data: {email: "email@.ru"}
+//       formData.append(key, options.data[key]);
+//     }
+//   }
+//   xhr.open(options.method, urlGet, true);
+//   xhr.responseType = 'json';
+//   xhr.send(formData);
+// };
  
-fetch
+// fetch
 
 const createRequest = (options = {}) => {
-  const fetchOptions = {//опции для формирования запроса
-    method,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+  // Создаем объект fetchOptions, который будет содержать параметры для запроса
+  const fetchOptions = {
+    method: options.method, // Метод запроса (GET, POST, и так далее)
   };
 
-  let requestUrl = url;//дубликат ссылки, безопасность кода
-  if (method === 'GET') {
-    const urlGet = new URL(url);
-    urlGet.search = new URLSearchParams(data).toString();//в search добавляю данные из data(пароль, емаил) и перевожу в строку
-    requestUrl = urlGet;
-  } else if (method === 'POST') {
-    fetchOptions.body = JSON.stringify(data);
+  const urlGet = options.url;
+
+  // Проверяем метод запроса
+  if (fetchOptions.method === 'GET') {
+    // Если метод GET, то формируем URL с параметрами из data
+    urlGet.search = new URLSearchParams(options.data).toString();
+  } else if (fetchOptions.method !== 'GET') {
+    // Если метод не GET, то формируем тело запроса (body)
+    const formData = new FormData();
+    for (const key in options.data) {
+      formData.append(key, options.data[key]);
+    }
+    // Устанавливаем тело запроса
+    fetchOptions.body = formData;
   }
-
-  fetch(requestUrl, fetchOptions)
-
+  // Выполняем AJAX-запрос с использованием fetch
+  fetch(urlGet, fetchOptions)
     .then((response) => {
+      // Проверяем, был ли успешный HTTP-статус
       if (!response.ok) {
         throw new Error(`Mistakes HTTP: ${response.status}`);
       }
+      // Возвращаем JSON-ответ
       return response.json();
     })
-
-    .then((responseData) => {
-      callback(null, responseData);
+    .then((result) => {
+      // Вызываем callback с данными ответа
+      options.callback(null, result);
     })
-
     .catch((error) => {
-      callback(error);
+      // В случае ошибки вызываем callback с ошибкой
+      options.callback(error);
     });
-
-}
-
-*/
+};
