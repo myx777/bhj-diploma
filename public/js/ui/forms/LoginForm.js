f6cfe9f812f0234d74d15655b4f34e2f05aed2b5
MYX <it.anton22@gmail.com>
2023-10-25 20:56:12 +0300

    add class loginform at loginForm.js

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -10,6 +10,14 @@ class LoginForm extends AsyncForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.login(data, (err, response) => {
+      if (response.success) {
+        App.setState('user-logged');
+        format.reset();
+        const modal = document.getElementById('modal-login');
+        modal.close();
+      }
+    });
   }
+  
 }
