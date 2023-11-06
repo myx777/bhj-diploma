e90d0543a43caa96dc72768f8494812aa6a79e08
MYX <it.anton22@gmail.com>
2023-11-07 00:24:53 +0300

    createrequest.js add creation url if method != get

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,4 +1,49 @@
 
+// /**
+//  * Основная функция для совершения запросов
+//  * на сервер.
+//  */
+// const createRequest = (options = {}) => {
+//   // Создаем объект fetchOptions, который будет содержать параметры для запроса
+//   const fetchOptions = {
+//     method: options.method, // Метод запроса (GET, POST, и так далее)
+//   };
+
+//   const urlGet = options.url;
+
+//   // Проверяем метод запроса
+//   if (fetchOptions.method === 'GET') {
+//     // Если метод GET, то формируем URL с параметрами из data
+//     urlGet.search = new URLSearchParams(options.data).toString();
+//   } else if (fetchOptions.method !== 'GET') {
+//     // Если метод не GET, то формируем тело запроса (body)
+//     const formData = new FormData();
+//     for (const key in options.data) {
+//       formData.append(key, options.data[key]);
+//     }
+//     // Устанавливаем тело запроса
+//     fetchOptions.body = formData;
+//   }
+//   // Выполняем AJAX-запрос с использованием fetch
+//   fetch(urlGet, fetchOptions)
+//     .then((response) => {
+//       // Проверяем, был ли успешный HTTP-статус
+//       if (!response.ok) {
+//         throw new Error(`Mistakes HTTP: ${response.status}`);
+//       }
+//       // Возвращаем JSON-ответ
+//       return response.json();
+//     })
+//     .then((result) => {
+//       // Вызываем callback с данными ответа
+//       options.callback(null, result);
+//     })
+//     .catch((error) => {
+//       // В случае ошибки вызываем callback с ошибкой
+//       options.callback(error);
+//     });
+// };
+
 /**
  * Основная функция для совершения запросов
  * на сервер.
@@ -9,13 +54,16 @@ const createRequest = (options = {}) => {
     method: options.method, // Метод запроса (GET, POST, и так далее)
   };
 
-  const urlGet = options.url;
+  const urlGet = new URL(options.url, 'http://localhost:8000');
 
   // Проверяем метод запроса
   if (fetchOptions.method === 'GET') {
     // Если метод GET, то формируем URL с параметрами из data
-    urlGet.search = new URLSearchParams(options.data).toString();
+
   } else if (fetchOptions.method !== 'GET') {
+    if (options.data && options.data.account_id) {
+      urlGet.searchParams.append('account_id', options.data.account_id);
+    }
     // Если метод не GET, то формируем тело запроса (body)
     const formData = new FormData();
     for (const key in options.data) {
@@ -24,6 +72,7 @@ const createRequest = (options = {}) => {
     // Устанавливаем тело запроса
     fetchOptions.body = formData;
   }
+
   // Выполняем AJAX-запрос с использованием fetch
   fetch(urlGet, fetchOptions)
     .then((response) => {
